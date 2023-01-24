30c8cbc7086da499a26f4dc4cc0d5e5c90cff543
MulisaD <111280627+MulisaD@users.noreply.github.com>
2023-01-24 14:23:46 +0200

    Add project files.

---
+++ Timesheet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you logging in ?, Please respond in Y for Yes and N for No");
+            var YesONo = Convert.ToString(Console.ReadLine());
+            if (YesONo == "Y")
+            {
+                Console.Write("Check in Date and Time is : ");
+                DateTime now = DateTime.Now;
+                Console.WriteLine(now);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Good bye");
+            }
+           
+        }
+    }
+}
